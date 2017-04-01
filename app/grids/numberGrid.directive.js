952d7cf9c72327a49a6a05bd31ed2b12ccbcedb5
DecaHub <dan@decahub.com>
2017-03-31 23:16:59 -0400

    Craete numberGrid directive

--- app/grids/numberGrid.directive.js
+++ app/grids/numberGrid.directive.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const NumberGridController = function (NumbersService) {
+	
+	const vm = this;
+	
+	vm.numbers = NumbersService.getNumbers();
+	
+};
+
+NumberGridController.$inject = ["NumbersService"];
+
 angular.module("grids")
 	.directive("dhNumberGrid", function () {
 		
@@ -7,7 +17,9 @@ angular.module("grids")
 			
 			restrict: "E",
 			templateUrl: "grids/numberGrid.template.html",
-			scope: {numbers: "<"}
+			scope: {numbers: "<"},
+			controller: NumberGridController,
+			controllerAs: "numGrid"
 			
 		};
 		
