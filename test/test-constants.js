d13de15998f8c2bd1307c61e29ceda121eca8dfb
DecaHub <dan@decahub.com>
2017-04-15 18:40:40 -0400

    Add the rest of the basic operations.

--- test/test-constants.js
+++ test/test-constants.js
@@ -16,8 +16,14 @@ for (let i = 0; i < numbersStrings.length; i++) {
 }
 
 ops.set("add", element(by.id("addition")));
+ops.set("minus", element(by.id("subtraction")));
+ops.set("times", element(by.id("multiplication")));
+ops.set("over", element(by.id("division")));
 ops.set("equal", element(by.id("resultant")));
+
+ops.set("clear", element(by.id("clear")));
 ops.set("posneg", element(by.id("posneg")));
+ops.set("percentage", element(by.id("percentage")));
 
 module.exports = {
 	
