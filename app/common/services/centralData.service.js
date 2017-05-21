31d13c5b654dbd964fabe98d6a9345ead9b9ae9d
DecaHub <dan@decahub.com>
2017-05-21 13:50:11 -0400

    Revamp services

--- app/common/services/centralData.service.js
+++ app/common/services/centralData.service.js
@@ -3,15 +3,31 @@
 angular.module("main")
 	.service("CentralDataService", function () {
 		
+		let core = this;
+		
+		core.currentDigit = undefined;
+		core.currentOp = undefined;
+		
+		core.numberStack = [];
+		core.opStack = [];
+		
 		this.storeCurrentNumber = function (num) {
 			
-			console.log(num);
+			core.currentDigit = num;
+			core.numberStack.push(num.data);
+			
+			console.log(core.currentDigit.data);
+			console.log(core.numberStack);
 			
 		};
 		
 		this.storeCurrentOp = function (op) {
 			
-			console.log(op);
+			core.currentOp = op;
+			core.opStack.push(op.label);
+			
+			console.log(core.currentOp.symbol);
+			console.log(core.opStack);
 			
 		};
 		
