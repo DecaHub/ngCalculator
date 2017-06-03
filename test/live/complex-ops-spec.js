d8470705bbed94c61ae86bb2cf3b917a3fb5f605
DecaHub <dan@decahub.com>
2017-06-03 15:58:49 -0400

    Add live tests.

---
+++ test/live/complex-ops-spec.js
@@ -0,0 +1,56 @@
+const dialer = require("./dialer").dialer;
+const tc = require("./test-constants");
+
+let performComplexOps = function (digitBox, numbers, ops) {
+	
+	describe("Perform complex operations", function () {
+		
+		it("2 + 2 = 4", function () {
+
+			dialer("2 + 2 = 4");
+
+			expect(digitBox.getText()).toEqual("4");
+
+		});
+
+		it(".25 + .85 - 9.97 * .2 = -0.894 = -0.894", function () {
+
+			dialer(".25 + .85 - 9.97 * .2 =");
+
+			expect(digitBox.getText()).toEqual("-0.894");
+
+		});
+
+		it("85.44 * 6244 + 32144 / 85244 / 96 +  should give 533487.36392794018738", function () {
+
+			dialer("85.44 * 6244 + 32144 / 85244 / 96 +");
+
+			expect(digitBox.getText()).toEqual("533487.36392794018738");
+
+		});
+		
+		
+		it("85.44 * 6244 + 32144 / 85244 / 96 + 22455 * 6 * 52 / 852 + 3695 = 545405.32167441906062", function () {
+
+			dialer("85.44 * 6244 + 32144 / 85244 / 96 + 22455 * 6 * 52 / 852 + 3695 =");
+
+			expect(digitBox.getText()).toEqual("545405.32167441906062");
+
+		});
+		
+	});
+	
+};
+
+
+describe("ngCalculator: Complex Operations", function () {
+	
+	beforeEach(function () {
+		
+		browser.get("https://decahub.github.io/ngCalculator/");
+		
+	});
+	
+	performComplexOps(tc.digitBox);
+	
+});
---
+++ test/live/order-operations-spec.js
@@ -0,0 +1,63 @@
+const tc = require("./test-constants");
+const dialer = require("./dialer").dialer;
+
+let performAdditions = function (digitBox, numbers, ops) {
+	
+	describe("Perform operations in right order", function () {
+		
+		it("2 + 3 * 4 * 10 = 122", function () {
+			
+			dialer("2 + 3 * 4 * 10 =");
+			
+			expect(digitBox.getText()).toEqual("122");
+			
+		});
+		
+		it("2 + 3 * 4 * 10 + should get 122", function () {
+			
+			dialer("2 + 3 * 4 * 10 +");
+			
+			expect(digitBox.getText()).toEqual("122");
+			
+		});
+		
+		it("2 + 3 * 4 * 10 - should get 122", function () {
+			
+			dialer("2 + 3 * 4 * 10 -");
+			
+			expect(digitBox.getText()).toEqual("122");
+			
+		});
+		
+		it("2 + 3 * 4 * 10 * should get 120", function () {
+			
+			dialer("2 + 3 * 4 * 10 *");
+			
+			expect(digitBox.getText()).toEqual("120");
+			
+		});
+		
+		it("2 + 3 * 4 * 10 - 10 / 2 * 4 = 102", function () {
+			
+			dialer("2 + 3 * 4 * 10 - 10 / 2 * 4 =");
+			
+			expect(digitBox.getText()).toEqual("102");
+			
+		});
+		
+	});
+	
+};
+
+
+describe("ngCalculator: Order of operations", function () {
+	
+	beforeEach(function () {
+		
+		browser.get("https://decahub.github.io/ngCalculator/");
+		
+	});
+	
+	performAdditions(tc.digitBox, tc.numbers, tc.ops);
+	
+});
